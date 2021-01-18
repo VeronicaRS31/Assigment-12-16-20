a1321252d012c9c014cfafad5fbd757e6d42b9ce
VeronicaRS31 <73490614+VeronicaRS31@users.noreply.github.com>
2021-01-17 22:34:35 -0800

    Update a121_catch_a_turtle_vr.py

--- a121_catch_a_turtle_vr.py
+++ a121_catch_a_turtle_vr.py
@@ -1,53 +1,44 @@
-# a121_catch_a_turtle.py
-#-----import statements-----
-import turtle as trtl
+#1.2.4 Turtle Escape
+import turtle as trtl 
 import random as rand
-
-#-----game configuration----
-spot_color = "pink"
-spot_shape="circle"
-spot_size=2
-#-----initialize turtle-----
-spot=trtl.Turtle()
-spot.shape(spot_shape)
-spot.turtlesize(spot_size)
-spot.fillcolor(spot_color)
-score_writer= trtl.Turtle()
-score_writer.penup()
-score_writer.goto(-200,200)
+walls=trtl.Turtle()
+walls.speed(0)
+walllenght=25
+doorwidth=15
+barrier_lenght=30
+maze_runner= trtl.Turtle(shape= "turtle")
+maze_runner.turtlesize(1)
+maze_runner.color("pink")
+maze_runner.hideturtle()
+walls.pencolor("black")
 font_setup = ("Arial", 20, "normal")
-timer = 30
+timer = 5
 counter_interval = 1000   #1000 represents 1 second
 timer_up = False
 counter =  trtl.Turtle()
 counter.penup()
-counter.goto(200,200)
-colors=["purple","yellow","blue","red","white","brown"]
-sizes=[0.5,1,1.5,2,2.5,3,3,5,4]
+counter.goto(400,300)
 
-#-----game functions--------
-font_setup=("Arial", 20, "normal")
-score=0
-def update_score():
-  global score 
-  score += 1
-  score_writer.clear()
-  score_writer.write("score:" + str(score), font=font_setup)
+def draw_door(x):
+  walls.forward(x)
+  walls.pu()
+  walls.forward(doorwidth)
+  walls.pd()
 
-def change_position():
-  new_xpos=rand.randint(-200,200)
-  new_ypos= rand.randint(-150,150)
-  spot.goto(new_xpos,new_ypos)
+def runner_down():
+  maze_runner.setheading(270)
 
-def spot_clicked(x,y):
-  if timer_up== False:
-     spot.penup()
-     spot.hideturtle()
-     change_position()
-     spot.showturtle()
-     update_score()
-     adding_color()
-     change_size()
+def runner_up():
+  maze_runner.setheading(90)
+
+def runner_left():
+  maze_runner.setheading(180)
+
+def runner_right():
+  maze_runner.setheading(0)
+
+def runner_move():
+  maze_runner.forward(5)
 
 def countdown():
   global timer, timer_up
@@ -55,30 +46,63 @@ def countdown():
   if timer <= 0:
     counter.write("Time's Up", font=font_setup)
     timer_up = True
+    wn.clear()
+    maze_runner.hideturtle()
   else:
     counter.write("Timer: " + str(timer), font=font_setup)
     timer -= 1
     counter.getscreen().ontimer(countdown, counter_interval) 
 
-
-
-def adding_color():
-  spot.fillcolor(rand.choice(colors))
-  spot.stamp()
-  spot.fillcolor("pink")
-
-def change_size():
-  spot.turtlesize(rand.choice(sizes))
-
+ 
 def start_game():
   if True:
-    spot.onclick(spot_clicked)
-    countdown()
+     countdown()
+
+for i in range(25):
+  walls.left(90)
+  if i<1:
+    walls.forward(walllenght)
+  elif i<5:
+    x= rand.randint(0,walllenght-doorwidth)
+    draw_door(x)
+    walls.forward(walllenght-x-doorwidth)
+  else:
+    if rand.randint(0,1)==1:
+      x= rand.randint(0,walllenght-doorwidth)
+      draw_door(x)
+      y= rand.randint(0,walllenght-doorwidth-x)
+      walls.forward(y)
+      walls.left(90)
+      walls.forward(barrier_lenght)
+      walls.backward(barrier_lenght)
+      walls.right(90)
+      walls.forward(walllenght-x-y-doorwidth)
+    else:
+      y= rand.randint(0,walllenght-doorwidth)
+      walls.forward(y)
+      walls.left(90)
+      walls.forward(barrier_lenght)
+      walls.backward(barrier_lenght)
+      walls.right(90)
+      x= rand.randint(0,walllenght-doorwidth-y)
+      draw_door(x)
+      walls.forward(walllenght-x-y-doorwidth)
+      walls.pd()
+  walllenght+=15
+  walls.hideturtle()
+  maze_runner.showturtle()
 
 start_game()
- 
-#-----events----------------
+
+
+
+wn=trtl.Screen()
+wn.onkeypress(runner_down,"a")
+wn.onkeypress(runner_up,"b")
+wn.onkeypress(runner_left,"c")
+wn.onkeypress(runner_right,"d")
+wn.onkeypress(runner_move,"g")
+wn.listen()
 
 wn = trtl.Screen()
-wn.bgcolor("teal")
 wn.mainloop()
